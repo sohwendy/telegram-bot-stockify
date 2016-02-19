145cf1c0507670ad92537684d416ff3ab88ff005
Wendy Soh <sohwendy@gmail.com>
2016-02-19 21:12:02 +0800

    send stock info to special user on weekend 9:05 am

---
+++ config/schedule.rb
@@ -0,0 +1,6 @@
+job_type :rake, "cd :path && rake ':task' :output"
+set :output, {:error => "tmp/cron_error_log.log", :standard => "tmp/cron_log.log"}
+
+every :weekday, :at => '9:05 am' do
+  rake 'bot:push'
+end
